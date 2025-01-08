3f8c041a44a47c0862e365525f319986e34ab2ac
dizon <dizon@DESKTOP-EL7HFPK>
2025-01-08 19:15:59 +0800

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿class Calculator
+{
+    public static double DoOperation(double num1, double num2, string op)
+    {
+        double result = double.NaN; 
+
+        switch(op)
+        {
+            case "a":
+                result = num1 + num2;
+                break;
+            case "s":
+                result = num1 - num2;
+                break;
+            case "m":
+                result = num1 * num2;
+                break;
+            case "d":
+                while (num2 == 0)
+                {
+                    Console.WriteLine("Please enter a non-zero divisor: ");
+                    result = num1 / num2;
+                   
+                }
+                break;
+            default:
+                break;
+
+        }
+        return result;
+    }
+}
